15fac8a608e3cea32a0982dd884cbdc37dc3a268
huyngse <ngogiahuy775309@gmail.com>
2024-11-30 15:35:04 +0700

    Update header

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,35 +1,47 @@
 import LogoImg from "@/assets/imgs/logo.png";
-import { Input } from "antd";
-import { SearchProps } from "antd/es/input";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { Link } from "react-router-dom";
 import { IoMdPerson } from "react-icons/io";
 import { FaShoppingCart } from "react-icons/fa";
+import { CiSearch } from "react-icons/ci";
 const Header = () => {
-  const { Search } = Input;
-  const onSearch: SearchProps["onSearch"] = (value, _e, info) =>
-    console.log(info?.source, value);
   return (
     <div className="bg-gray-900 text-white">
       <MaxWidthWrapper className="flex justify-between p-4 items-center">
         <div>
-          <Search
-            placeholder="SEARCH"
-            onSearch={onSearch}
-            style={{ width: 200 }}
-          />
+          <div className="bg-white rounded-md flex items-center py-1 px-2 gap-2">
+            <input
+              type="text"
+              className="text-gray-500 text-xs px-3 py-2 bg-transparent flex-1 focus:outline-double rounded"
+              placeholder="SEARCH..."
+            />
+            <button className="focus:outline-double p-1 rounded">
+              <CiSearch className="text-gray-500 h-4 w-4 " />
+            </button>
+          </div>
         </div>
         <div>
-          <img src={LogoImg} alt="" width={100} />
+          <Link to={"/"}>
+            <img src={LogoImg} alt="" width={100} />
+          </Link>
         </div>
         <div className="flex items-center gap-5">
           <div>
-            <Link to={"/sign-up"} className="text-sm">SIGN UP</Link>/
-            <Link to={"/login-in"} className="text-sm">LOG IN</Link>
+            <Link to={"/sign-up"} className="text-sm">
+              SIGN UP
+            </Link>
+            /
+            <Link to={"/login-in"} className="text-sm">
+              LOG IN
+            </Link>
           </div>
-          <div className="flex gap-2">
-            <button className="flex items-center"><IoMdPerson /></button>
-            <button className="flex items-center"><FaShoppingCart /></button>
+          <div className="flex gap-4">
+            <button className="flex items-center">
+              <IoMdPerson className="h-5 w-5"/>
+            </button>
+            <button className="flex items-center">
+              <FaShoppingCart className="h-5 w-5"/>
+            </button>
           </div>
         </div>
       </MaxWidthWrapper>
