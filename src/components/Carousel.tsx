081d9a730b93fa515a46fbcece08ca47afbb6a99
huyngse <ngogiahuy775309@gmail.com>
2024-12-01 20:53:17 +0700

    More carousel images

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,5 +1,10 @@
 import Slider from "react-slick";
-import CarouselImage from "@/assets/imgs/carousel_1.png";
+import CarouselImage1 from "@/assets/imgs/carousel_1.png";
+import CarouselImage2 from "@/assets/imgs/carousel_2.png";
+import CarouselImage3 from "@/assets/imgs/carousel_3.png";
+import CarouselImage4 from "@/assets/imgs/carousel_4.png";
+import CarouselImage5 from "@/assets/imgs/carousel_5.png";
+import CarouselImage6 from "@/assets/imgs/carousel_6.png";
 const Carousel = () => {
   const settings = {
     dots: true,
@@ -14,13 +19,22 @@ const Carousel = () => {
     <div className="slider-container">
       <Slider {...settings} >
         <div>
-          <img src={CarouselImage} alt="" className="h-[500px] w-full object-cover"/>
+          <img src={CarouselImage1} alt="" className="h-[500px] w-full object-cover"/>
         </div>
         <div>
-          <img src={CarouselImage} alt=""  className="h-[500px] w-full object-cover"/>
+          <img src={CarouselImage2} alt=""  className="h-[500px] w-full object-cover"/>
         </div>
         <div>
-          <img src={CarouselImage} alt=""  className="h-[500px] w-full object-cover"/>
+          <img src={CarouselImage3} alt=""  className="h-[500px] w-full object-cover"/>
+        </div>
+        <div>
+          <img src={CarouselImage4} alt=""  className="h-[500px] w-full object-cover"/>
+        </div>
+        <div>
+          <img src={CarouselImage5} alt=""  className="h-[500px] w-full object-cover"/>
+        </div>
+        <div>
+          <img src={CarouselImage6} alt=""  className="h-[500px] w-full object-cover"/>
         </div>
       </Slider>
     </div>
