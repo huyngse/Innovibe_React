cebb5c1a0455dbe1c1b4121a7878cb63b4b82eee
huyngse <ngogiahuy775309@gmail.com>
2024-12-04 15:53:35 +0700

    Improve Search bar

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,42 +1,55 @@
 import LogoImg from "@/assets/imgs/logo.png";
 import MaxWidthWrapper from "./MaxWidthWrapper";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { IoMdPerson } from "react-icons/io";
 import { FaShoppingCart } from "react-icons/fa";
 import { CiSearch } from "react-icons/ci";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { cn } from "@/lib/utils";
+import { Input } from "../ui/input";
+
 const Header = () => {
   const [isSticky, setIsSticky] = useState(false);
-
+  const searchInputRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
   const handleScroll = () => {
-      if (window.scrollY > 60) {
-          setIsSticky(true);
-      } else {
-          setIsSticky(false);
-      }
+    if (window.scrollY > 60) {
+      setIsSticky(true);
+    } else {
+      setIsSticky(false);
+    }
   };
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-        window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
-}, []);
+  }, []);
   return (
-    <header className={cn("bg-gray-900 text-white transition-all duration-300 z-50 -top-10 backdrop-blur-md", isSticky && "sticky top-0 shadow bg-gray-900/70")}>
+    <header
+      className={cn(
+        "bg-gray-900 text-white transition-all duration-300 z-50 -top-10 backdrop-blur-md",
+        isSticky && "sticky top-0 shadow bg-gray-900/70"
+      )}
+    >
       <MaxWidthWrapper className="flex justify-between p-4 items-center">
-        <div>
-          <div className="bg-white rounded-md flex items-center py-1 px-2 gap-2">
-            <input
-              type="text"
-              className="text-gray-500 text-xs px-3 py-2 bg-transparent flex-1 focus:outline-double rounded"
-              placeholder="SEARCH..."
-            />
-            <button className="focus:outline-double p-1 rounded">
-              <CiSearch className="text-gray-500 h-4 w-4 " />
-            </button>
-          </div>
-        </div>
+        <form
+          className="relative"
+          onSubmit={(e) => {
+            e.preventDefault();
+            navigate(`/search?keyword=${searchInputRef.current?.value}`);
+          }}
+        >
+          <Input
+            type="text"
+            placeholder="SEARCH..."
+            className="bg-white pe-8 text-black text-sm placeholder:text-xs"
+            ref={searchInputRef}
+          />
+          <button className="focus:outline-double p-1 rounded absolute right-2 top-1/2 -translate-y-1/2">
+            <CiSearch className="text-gray-500 h-4 w-4 " />
+          </button>
+        </form>
         <div>
           <Link to={"/"}>
             <img src={LogoImg} alt="" width={100} />
@@ -54,10 +67,10 @@ const Header = () => {
           </div>
           <div className="flex gap-4">
             <button className="flex items-center">
-              <IoMdPerson className="h-5 w-5"/>
+              <IoMdPerson className="h-5 w-5" />
             </button>
             <button className="flex items-center">
-              <FaShoppingCart className="h-5 w-5"/>
+              <FaShoppingCart className="h-5 w-5" />
             </button>
           </div>
         </div>
