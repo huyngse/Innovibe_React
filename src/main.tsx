34b2c9fdf58390f69ca78d1861a86a2ea0d6e167
huyngse <ngogiahuy775309@gmail.com>
2024-12-06 09:05:58 +0700

    Enable v7_relativeSplatPath & v7_startTransition

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,12 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter
+      future={{
+        v7_relativeSplatPath: true,
+        v7_startTransition: true,
+      }}
+    >
       <App />
     </BrowserRouter>
   </StrictMode>
