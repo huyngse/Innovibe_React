aecac5c08e53ce95f45d75e0640b41ebcea0f056
huyngse <ngogiahuy775309@gmail.com>
2024-12-06 11:04:35 +0700

    Comming soon image

--- src/components/shared/ComingSoon.tsx
+++ src/components/shared/ComingSoon.tsx
@@ -1,33 +1,13 @@
+import ComingSoonImage from "@/assets/imgs/comming-soon.png";
 const ComingSoon = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
-      <div className="text-center">
+      <div className="text-center flex flex-col items-center">
+        <img src={ComingSoonImage} alt="" width={250} />
         <h1 className="text-4xl font-bold text-gray-800 mb-4">Coming Soon!</h1>
         <p className="text-lg text-gray-600 mb-8">
           We're working hard to bring this feature to you. Stay tuned!
         </p>
-        <div className="flex justify-center">
-          <svg
-            className="animate-spin h-10 w-10 text-blue-500"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
-            ></circle>
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8v4a4 4 0 100 8v4a8 8 0 01-8-8z"
-            ></path>
-          </svg>
-        </div>
       </div>
     </div>
   );
