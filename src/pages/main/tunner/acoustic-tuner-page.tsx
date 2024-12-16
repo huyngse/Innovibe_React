ee2d9d9d0e76d893b47e155c2a653cbc668e58d5
huyngse <ngogiahuy775309@gmail.com>
2024-12-16 11:06:42 +0700

    Tuner page - note buttons for acoustic  tuner

--- src/pages/main/tunner/acoustic-tuner-page.tsx
+++ src/pages/main/tunner/acoustic-tuner-page.tsx
@@ -25,9 +25,73 @@ const AcousticTunerPage = () => {
           Chạm vào nốt nhạc, lên dây đàn theo đúng âm điệu.
         </p>
         <div className="grid grid-cols-12 gap-5">
-          <div className="col-span-6 flex justify-center">
-            <div className="w-[300px]">
-              <img src={guitarImage} alt="" className="w-full" />
+          <div className="col-span-6 relative">
+            <div className="absolute top-[85px] left-14">
+              <div className="flex flex-col gap-[52px]">
+                <div className="flex items-center">
+                  <Button
+                    className="shadow-lg rounded-full aspect-square flex justify-center items-center size-10"
+                    variant={"outline"}
+                  >
+                    <span className="text-lg font-bold text-orange-600">D</span>
+                  </Button>
+                  <div className="h-px border border-black w-[52px]" />
+                </div>
+                <div className="flex items-center">
+                  <Button
+                    className="shadow-lg rounded-full aspect-square flex justify-center items-center size-10"
+                    variant={"outline"}
+                  >
+                    <span className="text-lg font-bold text-orange-600">A</span>
+                  </Button>
+                  <div className="h-px border border-black w-[55px]" />
+                </div>
+                <div className="flex items-center">
+                  <Button
+                    className="shadow-lg rounded-full aspect-square flex justify-center items-center size-10"
+                    variant={"outline"}
+                  >
+                    <span className="text-lg font-bold text-orange-600">E</span>
+                  </Button>
+                  <div className="h-px border border-black w-[53px]" />
+                </div>
+              </div>
+            </div>
+            <div className="absolute top-[85px] right-12">
+              <div className="flex flex-col gap-[50px]">
+                <div className="flex items-center justify-end">
+                  <div className="h-px border border-black w-[53px]" />
+                  <Button
+                    className="shadow-lg rounded-full aspect-square flex justify-center items-center size-10"
+                    variant={"outline"}
+                  >
+                    <span className="text-lg font-bold text-orange-600">G</span>
+                  </Button>
+                </div>
+                <div className="flex items-center justify-end">
+                  <div className="h-px border border-black w-[59px]" />
+                  <Button
+                    className="shadow-lg rounded-full aspect-square flex justify-center items-center size-10"
+                    variant={"outline"}
+                  >
+                    <span className="text-lg font-bold text-orange-600">B</span>
+                  </Button>
+                </div>
+                <div className="flex items-center justify-end">
+                  <div className="h-px border border-black w-[58px]" />
+                  <Button
+                    className="shadow-lg rounded-full aspect-square flex justify-center items-center size-10"
+                    variant={"outline"}
+                  >
+                    <span className="text-lg font-bold text-orange-600">e</span>
+                  </Button>
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-center">
+              <div className="w-[300px]">
+                <img src={guitarImage} alt="" className="w-full" />
+              </div>
             </div>
           </div>
           <div className="col-span-6">
