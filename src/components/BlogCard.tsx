4e3e6687d5a6b4ef1488e0510c9aefbdcea6da65
huyngse <ngogiahuy775309@gmail.com>
2025-01-02 11:24:56 +0700

    feat(blog): related posts and featured posts in blog detail page

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -8,35 +8,66 @@ const BlogCard = ({
   createdAt,
   title,
   content,
+  variant = "default",
 }: {
   blogId: number;
   thumbnail: string;
   createdAt: string;
   title: string;
   content: string;
+  variant?: "small" | "default";
 }) => {
-  return (
-    <div>
-      <AspectRatio ratio={16 / 10}>
-        <img
-          src={thumbnail}
-          alt={title}
-          className="w-full h-full object-cover object-center"
-        />
-      </AspectRatio>
-      <p className="my-2 text-sm">{formatDate(new Date(createdAt))}</p>
-      <Link to={`/blog/${blogId}`}>
-        <h3 className="font-bold my-2">{title}</h3>
-      </Link>
-      <p className="text-sm text-gray-500">{content}</p>
-      <Link
-        to={`/blog/${blogId}`}
-        className="my-3 text-sm font-semibold underline"
-      >
-        Xem thêm
-      </Link>
-    </div>
-  );
+  if (variant == "default") {
+    return (
+      <div>
+        <AspectRatio ratio={16 / 10}>
+          <img
+            src={thumbnail}
+            alt={title}
+            className="w-full h-full object-cover object-center"
+          />
+        </AspectRatio>
+        <p className="my-2 text-sm">{formatDate(new Date(createdAt))}</p>
+        <Link to={`/blog/${blogId}`}>
+          <h3 className="font-bold my-2">{title}</h3>
+        </Link>
+        <p className="text-sm text-gray-500">{content}</p>
+        <Link
+          to={`/blog/${blogId}`}
+          className="my-3 text-sm font-semibold underline"
+        >
+          Xem thêm
+        </Link>
+      </div>
+    );
+  } else {
+    return (
+      <div className="grid grid-cols-3 gap-3">
+        <div className="col-span-1">
+          <AspectRatio ratio={16 / 10}>
+            <img
+              src={thumbnail}
+              alt={title}
+              className="w-full h-full object-cover object-center"
+            />
+          </AspectRatio>
+        </div>
+
+        <div className="col-span-2">
+          <Link to={`/blog/${blogId}`}>
+            <h3 className="font-bold text-sm">{title}</h3>
+          </Link>
+          <p className="text-sm">{formatDate(new Date(createdAt))}</p>
+          <Link
+            to={`/blog/${blogId}`}
+            className="text-sm font-semibold underline"
+          >
+            Xem thêm
+          </Link>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default BlogCard;
--- src/pages/main/blog-detail/blog-detail-page.tsx
+++ src/pages/main/blog-detail/blog-detail-page.tsx
@@ -3,6 +3,39 @@ import MaxWidthWrapper from "@/components/shared/MaxWidthWrapper";
 import TiptapView from "@/components/tiptap/TiptapView";
 import { mockBlog } from "./mock-blog";
 import { formatDateTime } from "@/utils/date";
+import blogImage1 from "@/assets/imgs/blog_1.png";
+import blogImage2 from "@/assets/imgs/blog_2.png";
+import blogImage3 from "@/assets/imgs/blog_3.png";
+import BlogCard from "@/components/BlogCard";
+const data = [
+  {
+    id: 1,
+    title:
+      "Fractal Audio Systems Launches VP4 A Direct Competitor to HX Effect, Eventide H90",
+    content:
+      "Fractal Audio Systems has just officially introduced VP4, a compact but powerful virtual pedalboard...",
+    createdAt: "2024-10-16",
+    thumbnail: blogImage1,
+  },
+  {
+    id: 2,
+    title:
+      "Fractal Audio Systems Launches VP4 A Direct Competitor to HX Effect, Eventide H90",
+    content:
+      "Fractal Audio Systems has just officially introduced VP4, a compact but powerful virtual pedalboard...",
+    createdAt: "2024-10-16",
+    thumbnail: blogImage2,
+  },
+  {
+    id: 3,
+    title:
+      "Fractal Audio Systems Launches VP4 A Direct Competitor to HX Effect, Eventide H90",
+    content:
+      "Fractal Audio Systems has just officially introduced VP4, a compact but powerful virtual pedalboard...",
+    createdAt: "2024-10-16",
+    thumbnail: blogImage3,
+  },
+];
 const BlogDetailPage = () => {
   return (
     <div>
@@ -13,7 +46,7 @@ const BlogDetailPage = () => {
         ]}
       />
       <MaxWidthWrapper className="py-10">
-        <div className="grid grid-cols-12">
+        <div className="grid grid-cols-12 gap-5">
           <div className="col-span-8">
             <div className="pb-5">
               <h1 className="text-4xl font-bold">{mockBlog.title}</h1>
@@ -24,8 +57,46 @@ const BlogDetailPage = () => {
               <img src={mockBlog.thumbnail} className="w-full" />
             </div>
             <TiptapView value={mockBlog.content} />
+            <div className="py-10">
+              <h3 className="text-xl font-bold border-b border-orange-500 py-3">
+                Các bài đăng liên quan
+              </h3>
+              <div className="grid grid-cols-3 py-3 gap-3">
+                {data.map((blog, index: number) => {
+                  return (
+                    <BlogCard
+                      blogId={blog.id}
+                      content={blog.content}
+                      createdAt={blog.createdAt}
+                      title={blog.title}
+                      thumbnail={blog.thumbnail}
+                      key={index}
+                    />
+                  );
+                })}
+              </div>
+            </div>
           </div>
-          <div></div>
+          <div className="col-span-4">
+            <h3 className="text-xl font-bold border-b border-orange-500 py-3">
+              Bài viết nổi bật
+            </h3>
+            <div className="flex flex-col gap-3 py-3">
+              {data.map((blog, index: number) => {
+                return (
+                  <BlogCard
+                    blogId={blog.id}
+                    content={blog.content}
+                    createdAt={blog.createdAt}
+                    title={blog.title}
+                    thumbnail={blog.thumbnail}
+                    key={index}
+                    variant="small"
+                  />
+                );
+              })}
+            </div>
+          </div>
         </div>
       </MaxWidthWrapper>
     </div>
