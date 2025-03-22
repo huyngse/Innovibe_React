162fb5e2e212d78575c78c63a11b485e206c2e37
huyngse <ngogiahuy775309@gmail.com>
2025-03-22 08:11:54 +0700

    chore(deployment): trigger build

--- src/stores/use-product-store.ts
+++ src/stores/use-product-store.ts
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 import { getAllProduct, getProductById, searchProducts } from '@/lib/api/product-api';
 import { Product } from '@/types/product';
-
+// 
 interface ProductState {
     products: Product[];
     product?: Product;
